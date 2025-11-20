cd984dcfbf64ad57afbd97ffe8dc6879bbe44b06
Rodrigo-Silva-Romero <rodrigosilvaromero14@gmail.com>
2025-11-19 22:02:36 -0300

    Se agrega los piplines para levantar y destruir la infra, ademas se suma la vinculacion del bucket

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -6,6 +6,12 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "bucket-tf"
+    key    = "root/terraform.tfstate"      # la ruta + nombre del archivo dentro del bucket
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
