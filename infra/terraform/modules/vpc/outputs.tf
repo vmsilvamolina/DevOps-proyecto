487f6bacdcbf2cda2f74e90308213da132d62930
Rodrigo Silva <153576329+Rodrigo-Silva-Romero@users.noreply.github.com>
2025-11-23 00:39:32 -0300

    se borra de outputs vpc subnet privadas

--- infra/terraform/modules/vpc/outputs.tf
+++ infra/terraform/modules/vpc/outputs.tf
@@ -8,11 +8,6 @@ output "public_subnet_ids" {
   value       = aws_subnet.public.*.id
 }
 
-output "private_subnet_ids" {
-  description = "List of private subnet IDs"
-  value       = aws_subnet.private.*.id
-}
-
 output "internet_gateway_id" {
   description = "Internet Gateway ID"
   value       = aws_internet_gateway.this.id
