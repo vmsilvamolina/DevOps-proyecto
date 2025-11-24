827f4b100aefa86b910d61e215c42db64aac6cd5
Rodrigo Silva <153576329+Rodrigo-Silva-Romero@users.noreply.github.com>
2025-11-23 21:33:31 -0300

    Update test_product_service.py

--- tests/test_product_service.py
+++ tests/test_product_service.py
@@ -1,17 +1,18 @@
 # tests/test_product_service.py
 import sys
+import pytest
 sys.path.append("product-service")  # Agrega la carpeta del microservicio al path
 
-from fastapi.testclient import TestClient
 import main
+from httpx import AsyncClient
 
-def test_health_endpoint():
-    client = TestClient(main.app)
-    response = client.get("/health")
-
-    assert response.status_code == 200
-    data = response.json()
-
-    assert "status" in data
-    assert data["status"] == "healthy"
-    assert data["service"] == "product-service"
+@pytest.mark.asyncio
+async def test_health_endpoint():
+    async with AsyncClient(app=main.app, base_url="http://testserver") as client:
+        response = await client.get("/health")
+        
+        assert response.status_code == 200
+        data = response.json()
+        assert "status" in data
+        assert data["status"] == "healthy"
+        assert data["service"] == "product-service"
