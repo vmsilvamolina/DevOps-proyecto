98d2ccf9de13c0ad726a3482b829f9186fdc1f35
Rodrigo Silva <153576329+Rodrigo-Silva-Romero@users.noreply.github.com>
2025-11-23 22:05:25 -0300

    Update test_product_service.py

--- tests/test_product_service.py
+++ tests/test_product_service.py
@@ -1,11 +1,17 @@
 # tests/test_product_service.py
+
+import sys
+import os
+# Agregar la carpeta del servicio al path para poder importar main.py
+sys.path.append(os.path.join(os.path.dirname(__file__), "../product-service"))
+
 import pytest
 from httpx import AsyncClient
-from main import app
+import main  # ahora Python encuentra main.py aunque la carpeta tenga guion
 
 @pytest.mark.asyncio
 async def test_health_endpoint():
-    async with AsyncClient(app=app, base_url="http://test") as client:
+    async with AsyncClient(app=main.app, base_url="http://test") as client:
         response = await client.get("/health")
     
     assert response.status_code == 200
