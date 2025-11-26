561443ef07dccfaf9bfe23409f731000f040ec23
Rodrigo Silva <153576329+Rodrigo-Silva-Romero@users.noreply.github.com>
2025-11-26 18:31:50 -0300

    Update outputs.tf

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -27,3 +27,16 @@ output "ecs_service_names" {
   description = "Nombre del ECS Service creado"
   value       = module.ecs_service_core.service_names
 }
+
+# ECR individual outputs para uso con terraform -raw
+output "ecr_api_url" {
+  value = module.ecr.ecr_urls["apigateway"]
+}
+
+output "ecr_product_url" {
+  value = module.ecr.ecr_urls["product"]
+}
+
+output "ecr_inventory_url" {
+  value = module.ecr.ecr_urls["inventory"]
+}
